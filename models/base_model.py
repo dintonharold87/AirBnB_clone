623ab545cdfc87dd2baac3a778f4d4139ecf5dd6
Dinton Harold Ainemukama <dintonharold@gmail.com>
2022-10-26 17:14:54 +0300

    Linked BaseModel to FileStorage by using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ Contains the Base class for the AirBnB clone console.
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -32,6 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
@@ -45,6 +47,7 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
